efd0990ed08c58be159af61462202c164dea9670
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-11-21 21:03:14 -0600

    data should work but CORS

---
+++ src/data/productData.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const baseUrl = 'https://localhost:44319/api';
+
+const getAllProducts = () => new Promise((resolve, reject) => {
+    axios.get(`${baseUrl}/products`)
+        .then(result => resolve(result.data))
+        .catch(err => reject(reject))
+})
+
+export default {getAllProducts};
