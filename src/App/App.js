6c4ec04d8cb388e62ca9a1b342665c6ee64f0ecf
Heath Moore <heathjhmoore@gmail.com>
2019-11-23 13:56:36 -0600

    created start of navbar

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import './App.scss';
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 
-import Home from '../Home/Home';
+import Home from '../Components/Home/Home';
+import MyNavbar from '../Components/Navbar/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <Home/>
-    </div>
+const PublicRoute = ({ component: Component, authed, ...rest}) => {
+  const routeChecker = props => ( authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/Home', state: { from: props.location }}}/>)
   );
+  return <Route {...rest} render={props => routeChecker(props)}/>
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest}) => {
+  const routeChecker = props => ( authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/MyProfile', state: { from: props.location }}}/>)
+  );
+  return <Route {...rest} render={props => routeChecker(props)}/>
+};
+
+class App extends React.Component {
+
+  state = {
+    authed : false
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <MyNavbar authed={this.state.authed}/>
+            <Switch>
+              <PublicRoute path='/Home' component={Home} authed={this.state.authed}/>
+              {/* <PublicRoute path='/CreateUser' component={CreateUser} authed={authed}/> */}
+              {/* <PrivateRoute path='/MyProfile' component={MyProfile} authed={authed}/> */}
+              <Redirect from="*" to="/Home"/>
+            </Switch>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
