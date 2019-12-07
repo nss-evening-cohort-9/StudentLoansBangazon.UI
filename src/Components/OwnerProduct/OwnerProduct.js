a85df10f80395308ad362cbaf33d45f02da08fa4
Heath Moore <heathjhmoore@gmail.com>
2019-12-07 12:34:39 -0600

    building card information out

--- src/Components/OwnerProduct/OwnerProduct.js
+++ src/Components/OwnerProduct/OwnerProduct.js
@@ -10,9 +10,18 @@ class OwnerProduct extends React.Component {
                     <img id="ownerProductCard-productImage"src={this.props.ownerProduct.imageUrl}/>
                 </div>
                 <div id="ownerProductCard-rightColumn">
-                    <h5>{this.props.ownerProduct.startDate}</h5>
-                    <h5>{this.props.ownerProduct.returnedDate}</h5>
-                    <h5>{this.props.ownerProduct.pricePerDay}</h5>
+                    <div>
+                        <h4>Rental Start Date</h4>
+                        <h5>{this.props.ownerProduct.startDate}</h5>
+                    </div>
+                    <div>
+                        <h4>Rental Return Date</h4>
+                        <h5>{this.props.ownerProduct.returnedDate}</h5>
+                    </div>
+                    <div>
+                        <h4>Price Per Day</h4>
+                        <h5>{this.props.ownerProduct.pricePerDay}</h5>
+                    </div>
                 </div>
             </div>
         )
